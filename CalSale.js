57ac2859f04292830c4f47748c2b49e4fc9dd918
nurizati <91452441+nurizati@users.noreply.github.com>
2021-12-17 15:49:06 +0800

    task3

--- CalSale.js
+++ CalSale.js
@@ -2,7 +2,12 @@ document.getElementById("output").style.display = "none";
 
 function kira() {
     let name = document.getElementById("i_name").value;
+  //  let membercard = document.getElementsByName("membercard").value;
+
     document.getElementById("o_name").innerText = name;
+   // document.getElementById("o_mc").innerText = membercard;
+
     document.getElementById("input").style.display = "none";
     document.getElementById("output").style.display = "table";
 }
+
