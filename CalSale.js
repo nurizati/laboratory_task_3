41af2f1b23e0aeb3033749393bad81ac60459593
nurizati <91452441+nurizati@users.noreply.github.com>
2021-12-17 16:59:08 +0800

    TASK3

--- CalSale.js
+++ CalSale.js
@@ -2,10 +2,16 @@ document.getElementById("output").style.display = "none";
 
 function kira() {
     let name = document.getElementById("i_name").value;
-  //  let membercard = document.getElementsByName("membercard").value;
+    let yes = document.getElementById("yes").checked;
+    let no = document.getElementById("no").checked;
+    let name1 = document.getElementById("i_name1").value;
+    let unitprice = document.getElementById("i_unitprice").value;
 
     document.getElementById("o_name").innerText = name;
-   // document.getElementById("o_mc").innerText = membercard;
+    document.getElementById("o_yes").checked = yes;
+    document.getElementById("o_no").checked = no;
+    document.getElementById("o_name1").innerText = name1;
+    document.getElementsByName("o_unitprice").innerText = unitprice;
 
     document.getElementById("input").style.display = "none";
     document.getElementById("output").style.display = "table";
